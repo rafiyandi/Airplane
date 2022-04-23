dc318b99dd6b5d8d12c436366287780ee0c224a0
rafiyandi <rafiyandi1905@gmail.com>
2022-04-24 02:07:12 +0700

    slacing sign-in

--- app_travel/lib/ui/page/sign_up_page.dart
+++ app_travel/lib/ui/page/sign_up_page.dart
@@ -17,7 +17,7 @@ class SignUpPage extends StatelessWidget {
     }
 
     Widget inputSection() {
-      Widget emailinput() {
+      Widget fullNameInput() {
         return Container(
           width: double.infinity,
           height: 82,
@@ -48,6 +48,123 @@ class SignUpPage extends StatelessWidget {
         );
       }
 
+      Widget emailInput() {
+        return Container(
+          margin: EdgeInsets.only(top: 20),
+          width: double.infinity,
+          height: 82,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Email Address",
+                style: blackTextStyle,
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Your Email",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                          borderSide: BorderSide(color: kPrimaryColor))),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget passwordInput() {
+        return Container(
+          margin: EdgeInsets.only(top: 20),
+          width: double.infinity,
+          height: 82,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Password",
+                style: blackTextStyle,
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Expanded(
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Your Password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                          borderSide: BorderSide(color: kPrimaryColor))),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget hobbyInput() {
+        return Container(
+          margin: EdgeInsets.only(top: 20),
+          width: double.infinity,
+          height: 82,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hobby",
+                style: blackTextStyle,
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Your Hobby",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                          borderSide: BorderSide(color: kPrimaryColor))),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget submitButton() {
+        return Container(
+          margin: EdgeInsets.only(top: 30),
+          width: double.infinity,
+          height: 55,
+          child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: kPrimaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  )),
+              onPressed: () {},
+              child: Text(
+                "Get Started",
+                style:
+                    whiteTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+              )),
+        );
+      }
+
       return Container(
         margin: EdgeInsets.only(top: 30),
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
@@ -57,8 +174,23 @@ class SignUpPage extends StatelessWidget {
         ),
         child: Column(
           children: [
-            emailinput(),
+            fullNameInput(),
+            emailInput(),
+            passwordInput(),
+            hobbyInput(),
+            submitButton(),
           ],
+        ),
+      );
+    }
+
+    Widget tcButton() {
+      return Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(top: 50, bottom: 73),
+        child: Text(
+          "Terms and Conditions",
+          style: greyTextStyle.copyWith(fontSize: 16, fontWeight: light),
         ),
       );
     }
@@ -71,6 +203,7 @@ class SignUpPage extends StatelessWidget {
           children: [
             title(),
             inputSection(),
+            tcButton(),
           ],
         ),
       ),
